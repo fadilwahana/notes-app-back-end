20b6c86d15ca562fbd67dd32f4f938ca48b4f7b5
fadilwahana98@gmail.com <fadilwahana98@gmail.com>
2023-03-20 14:56:36 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+/*eslint-disable*/
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.44.174',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
